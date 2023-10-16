9db3f67e8bdb21ea7beb0eb07ccc876c86b09b69
SharbelOkzan <sharbelpib@gmail.com>
2023-10-17 01:11:19 +0200

    configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:easy_poker/firebase_options.dart';
 import 'package:easy_poker/src/core/presentation/view/pages/game_page.dart';
 import 'package:easy_poker/src/service_locator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   configureDependencies();
   runApp(const MyApp());
 }
