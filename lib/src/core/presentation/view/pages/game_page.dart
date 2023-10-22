f4560fa9af66572579362b28da7652419333d880
SharbelOkzan <sharbelpib@gmail.com>
2023-10-22 16:29:18 +0200

    completed UI implementation

--- lib/src/core/presentation/view/pages/game_page.dart
+++ lib/src/core/presentation/view/pages/game_page.dart
@@ -2,6 +2,7 @@ import 'package:easy_poker/src/core/domain/entities/enums/game_phase.dart';
 import 'package:easy_poker/src/core/domain/logic/controllers/offline_game_cotroller.dart';
 import 'package:easy_poker/src/core/presentation/notifiers/game_notifier.dart';
 import 'package:easy_poker/src/core/presentation/notifiers/selected_cards_for_exchange_notifier.dart';
+import 'package:easy_poker/src/core/presentation/view/widgets/game_controls_widget.dart';
 import 'package:easy_poker/src/core/presentation/view/widgets/game_results_widget.dart';
 import 'package:easy_poker/src/core/presentation/view/widgets/hand_widget.dart';
 import 'package:flutter/material.dart';
@@ -24,74 +25,62 @@ class GamePage extends ConsumerWidget {
       }
     });
     return Scaffold(
-      body: Center(
-        child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _GameButtons(
-                ref: ref,
-              ),
-              _gameBody(ref),
-            ]),
-      ),
+      body: _getGameBodyAndControls(ref),
     );
   }
 
-  Widget _gameBody(WidgetRef ref) {
+  _getGameBodyAndControls(WidgetRef ref) {
+    return Center(
+      child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GameControls(
+                ref.watch(gameNotifierProvider).phase as OfflineGamePhase,
+                offlineGameControllerProvider),
+            _getGameBody(ref),
+          ]),
+    );
+  }
+
+  Widget _getGameBody(WidgetRef ref) {
     OfflineGamePhase phase =
         ref.watch(gameNotifierProvider).phase as OfflineGamePhase;
     switch (phase) {
       case OfflineGameRunning():
-        return HandWidget(
-          selectedCardsForExchangeIndecies:
-              ref.watch(selectedCardsForExchangeProvider).selectedCards,
-          cards: ref
-              .watch(offlineGameControllerProvider)
-              .currentActivePlayer
-              ?.cards,
-          onCardTap: ref
-              .read(selectedCardsForExchangeProvider.notifier)
-              .selectCardForExchange,
-        );
+        bool hasActivePlayer =
+            ref.watch(offlineGameControllerProvider).currentActivePlayer !=
+                null;
+
+        return (hasActivePlayer)
+            ? _getHand(ref)
+            : const _BetweenTurnsInstructionsWidget();
       case GameEndedPhase():
         return GameResultsWidget(phase: phase);
     }
   }
+
+  HandWidget _getHand(WidgetRef ref) {
+    return HandWidget(
+      selectedCardsForExchangeIndecies:
+          ref.watch(selectedCardsForExchangeProvider).selectedCards,
+      cards:
+          ref.watch(offlineGameControllerProvider).currentActivePlayer?.cards,
+      onCardTap: ref
+          .read(selectedCardsForExchangeProvider.notifier)
+          .selectCardForExchange,
+    );
+  }
 }
 
-class _GameButtons extends StatelessWidget {
-  final WidgetRef ref;
-  const _GameButtons({
-    super.key,
-    required this.ref,
-  });
+class _BetweenTurnsInstructionsWidget extends StatelessWidget {
+  const _BetweenTurnsInstructionsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 42.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          if (ref.watch(gameNotifierProvider).phase is! GameEndedPhase)
-            TextButton(
-                onPressed:
-                    ref.read(offlineGameControllerProvider).endCurrentGamePhase,
-                child: const Text("Done")),
-          if (ref.watch(selectedCardsForExchangeProvider).canExchange)
-            TextButton(
-                onPressed: _onExchangeCardsPressed,
-                child: const Text("Exchange selected")),
-        ],
-      ),
+    return const Center(
+      child: Text("Pass the phone to the next player"),
     );
-  }
-
-  void _onExchangeCardsPressed() {
-    ref.read(offlineGameControllerProvider).exchangeCards(
-        ref.read(selectedCardsForExchangeProvider).selectedCards);
-    ref.read(selectedCardsForExchangeProvider.notifier).onCardsExchanged();
   }
 }
